4a2bc186171ac91f0fa571e7590fd70d8a3c90f3
naveed530 <bitf12m530@pucit.edu.pk>
2015-06-16 03:13:17 -0700

    my first updation

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_Practice5_Template.MvcApplication" Language="C#" %>
---
+++ Scripts/showHide.js
@@ -0,0 +1,37 @@
+﻿function OpenClicked() {
+            //$('#tmpDialogue').show('slow');
+            //$('#tmpDialogue').slideDown('slow');
+            $('#box1').fadeIn('slow');
+
+        }
+function CloseClicked() {
+    // $('#tmpDialogue').hide(50000);
+    //  $('#tmpDialogue').slideUp(5000);
+    $('#box1').fadeOut('slow');
+}
+$(document).ready(function () {
+
+    $('#box1').hide();
+
+    $('#open1').click(OpenClicked);
+    $('#close1').click(CloseClicked);
+});
+
+function OpenClicked() {
+    //$('#tmpDialogue').show('slow');
+    //$('#tmpDialogue').slideDown('slow');
+    $('#box2').fadeIn('slow');
+
+}
+function CloseClicked() {
+    // $('#tmpDialogue').hide(50000);
+    //  $('#tmpDialogue').slideUp(5000);
+    $('#box2').fadeOut('slow');
+}
+$(document).ready(function () {
+
+    $('#box2').hide();
+
+    $('#open2').click(OpenClicked);
+    $('#close2').click(CloseClicked);
+});
